1e6d745e3445e07598902c609d541224b94ada7e
Dan Feng <d.feng@hotmail.com>
2024-07-25 16:51:40 +0000

    Add post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = False    
 
 ALLOWED_HOSTS = ['8000-crazyeggz-cidjangoblog-mrsvo11iygz.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
---
+++ staticfiles/js/script.js
@@ -0,0 +1,1 @@
+console.log("Didi You can nail the danm Django!!")
