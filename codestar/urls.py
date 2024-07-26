ace301691e66254cddf7a0ec276774b0d2593957
Dan Feng <d.feng@hotmail.com>
2024-07-26 14:41:44 +0000

    Add authentication

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('about/', include('about.urls')),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include("blog.urls"), name = "blog-urls"), # name attribute is not supposed to be used here. Name shoud be used for a specific url pattern rather than "include"
